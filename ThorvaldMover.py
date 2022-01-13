8dc545ec5c5e88451897494ad5e75c5796a59441
James-ben-tombling <92380630+James-ben-tombling@users.noreply.github.com>
2022-01-13 14:37:05 +0000

    Add files via upload

--- ThorvaldMover.py
+++ ThorvaldMover.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
@@ -27,13 +27,23 @@ class Mover:
         """
         Callback called any time a new laser scan becomes available
         """
-
+        end = 390
+        start = 330
         rospy.loginfo(
             rospy.get_caller_id() + "I heard %s", data.header.seq)
-        min_dist = min(data.ranges)
+        min_dist = min(data.ranges[start:end])
+        # min angle -2.18 rad/ -125 deg
+        #  max 2.359 rad /130 deg
+        # min_angle = data.angle_min
+        # max_angle = data.angle_max
+        # angle_increments = data.angle_increment
+        # angle_range = max_angle - min_angle
+        # angle_index = angle_range/angle_increments
         t = Twist()
+        # print(angle_index)
         if min_dist < 4:
             t.angular.z = 1.0
+            # t.linear.x = 0.8 
         else:
             t.linear.x = 0.8
         self.publisher.publish(t)
@@ -42,3 +52,4 @@ if __name__ == '__main__':
     rospy.init_node('mover')
     Mover()
     rospy.spin()
+
